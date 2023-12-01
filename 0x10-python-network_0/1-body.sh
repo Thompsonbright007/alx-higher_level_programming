7f5e85975b3e5428af5ca52aaad49ced4276f812
thompsonbright007 <brightthompson35@gmail.com>
2023-11-30 18:08:50 -0800

    body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Script to display content if request successfull
-curl -sf $1 2>/dev/null
+#!/bin/bash;
+# Send GET request and display body for 200 status
+[ $# -eq 0 ] && { echo "Usage: $0 <URL>"; exit 1; }; response=$(curl -s -w "%{http_code}" "$1"); [ "$(tail -n 1 <<< "$response")" == "200" ] && echo "$(head -c -4 <<< "$response")" || echo "Error: HTTP status code $(tail -n 1 <<< "$response")"
